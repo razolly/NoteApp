464ea0ff4ce7e35a78a9fb3f57d70e9ee39bcdb9
Raz <razli1993@hotmail.com>
2018-06-20 15:36:28 +0800

    Added new Activity to handle editing of note

--- app/src/main/java/com/example/razli/noteapp/MainActivity.java
+++ app/src/main/java/com/example/razli/noteapp/MainActivity.java
@@ -1,16 +1,37 @@
 package com.example.razli.noteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView noteListView;
+    ArrayList<String> mNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        noteListView = findViewById(R.id.noteListView);
+
+        mNotes = new ArrayList<String>();
+        mNotes.add("Example Note");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mNotes);
+        noteListView.setAdapter(arrayAdapter);
+        noteListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Start new activity
+                Intent intent = new Intent(getApplicationContext(), )
+            }
+        });
     }
 }
